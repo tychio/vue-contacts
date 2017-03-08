1f60bb9b56c8f954bf2c83baa0d3ecedf58d5013
Tychio <code@tychio.net>
2017-03-08 23:04:47 +0800

    ZZ - remove title component

--- src/app/Title.spec.js
+++
@@ -1,7 +0,0 @@
-import Title from './Title.vue';
-
-describe('Title', () => {
-  it('should be a title', () => {
-    expect(Title.name).toEqual('Title');
-  });
-});
--- src/app/components/Main.vue
+++ src/app/components/Main.vue
@@ -1,18 +1,15 @@
 <template>
 <main>
-  <title-component></title-component>
   <contacts></contacts>
 </main>
 </template>
 
 <script>
-import Title from '../Title.vue';
 import Contacts from './Contacts.vue';
 
 export default {
   name: 'Main',
   components: {
-    'title-component': Title,
     'contacts': Contacts
   }
 };
